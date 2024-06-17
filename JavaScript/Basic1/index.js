3726f3d67c08b86e4fddbbb54d9bf91ba8f690eb
KULBHUSHANSINGH-arch <gautamenergy526@gmail.com>
2024-06-17 10:08:39 +0530

    JavaScript

---
+++ JavaScript/Basic1/index.js
@@ -0,0 +1,39 @@
+/**Comment Line like this  */
+
+console.log('Guru Jee I am Dedicated for Learnig');
+console.log('Start Variables');
+/** Variables  */
+
+/** Create Variable by "let" */
+let a = 5;
+console.log(a);
+let b = 'Love';
+console.log(b);
+
+let c = true;
+console.log(c);
+
+/** Create Variable by "Var" */
+var x = 33;
+var y = 'Sonam Developer';
+var status = false;
+
+console.log(x);
+console.log(y);
+console.log(status);
+
+/** Create Variable by "Var" */
+const d = 34;
+
+/**
+ * 
+ 'We can not use the reserved keyword'
+ can not start the Number
+ Create meanining full 
+ can not start Contain Space or '_'
+ camaleCase use also
+
+
+ */
+
+/**   Primitive Types */
